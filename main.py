25f220235b6bb755eb013f56f426eeb99e15316e
Pedro <pedrohenriquefa97@gmail.com>
2025-03-27 18:54:02 -0300

    Implement letter validation and tracking in hangman game

--- main.py
+++ main.py
@@ -12,13 +12,44 @@ print(f'Você tem {vidas} vidas.')
 aux = '*' * len(palavra)  # Cria uma string auxiliar com o mesmo tamanho da palavra secreta
 print(f'A palavra é {aux}')
 
+usadas = []  # Lista para armazenar as letras já digitadas
+
 # Iniciar o loop do jogo
 while True:
     # Permitir uma letra para avariavel var
     var =  input('Insira uma letra: ')
-    # verificar se var ja foi digitada
     
+    # verificar se var ja foi digitada
+    if var in usadas:  # O 'in' quer dizer que vou na lista usadas e verifico se a letra ja foi digitada
+        print('A letra {var} já foi digitada, tente outra.')
+        continue  # Se a letra já foi digitada, continua o loop sem diminuir vidas
+    else:
+        usadas.append(var) # Adiciona a letra digitada na lista usadas
+        
     # verificar se var esta na palavra
+    if var in palavra:
+        print(f'A letra {var} esta na palavra!')
+        frase = ''
+        for letra in palavra:
+            if letra in usadas:
+                aux = letra
+            else: 
+                aux = '*'
+            frase = frase + aux
+        print(f'A palavra é: {frase}') 
+    else:
+        print(f'A letra {var} não está na palavra. Tente novamente.')
+        
+        if vidas < 0:
+            vidas -= 1  # Se a letra não estiver na palavra, diminui uma vida	
+        if vidas == 0:
+            print('Você perdeu!')
+            print(f'A palavra era: {palavra}')
+            break
+        print('Agora você tem {vidas} vidas.')
+    print(f' As letras que ja foram usadas são: {usadas}')
+    
+print ('Jogo Finalizado!')
     
     # verificar se ainda temos vidas
     
